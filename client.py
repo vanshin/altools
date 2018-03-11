93f9b9d78db9c250d82b754d45bf977f00fa12ca
vanshin <kfx721@hotmail.com>
2018-03-12 00:36:48 +0800

    update

--- client.py
+++ client.py
@@ -6,6 +6,24 @@ import time
 import requests
 
 class HttpClient(object):
-    def __init__():
-        pass
+    def __init__(self, server):
+        self.address = server['address']
+        self.port = server['port']
+        self.server = 'http://{}:{}/'.format(self.address, self.port)
 
+    def get(self, path, data={}):
+        ret = requests.get(self.server+path, data)
+        return ret.json()
+
+    def post(self, path, data={}):
+        ret = requests.post(self.server+path, data)
+        return ret.json()
+
+if __name__ == '__main__':
+    server = {
+        'address': '116.196.113.214',
+        'port': '5000'
+    }
+    hc = HttpClient(server)
+    ret = hc.get('ping')
+    print(ret)
